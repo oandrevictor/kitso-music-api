f50a0e4592c78f0925e4d3439e68d161e97da09c
André Andrade <oandrevictor@gmail.com>
2018-11-28 17:53:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,11 +16,21 @@ var db_url;
 (ENV == 'production')? db_url = db.url : db_url = db.local_url;
 mongoose.connect(db_url);
 
+var allowedOrigins = ['http://localhost:3000'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}))
+
 // Settings
-app.all("/*", function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-})
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
